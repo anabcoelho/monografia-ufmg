40205a00491ea23f267cf27542a66dad5d026fcf
Ana Coelho <anabcoelh@gmail.com>
2026-04-12 17:06:52 -0300

    firstcommit

---
+++ .Rprofile
@@ -0,0 +1,5 @@
+# Biblioteca do usuário
+.libPaths(Sys.getenv("R_LIBS_USER"))
+
+# CRAN confiável
+options(repos = c(CRAN = "https://cloud.r-project.org"))
---
+++ 0_testes/teste.r
@@ -0,0 +1,8 @@
+library(reticulate)
+
+use_python(
+  "C://Users//T-GAMER//OneDrive//Documentos//GitHub//monografia-ufmg//.venv//Scripts//python.exe",
+  required = TRUE
+)
+
+py_config()
